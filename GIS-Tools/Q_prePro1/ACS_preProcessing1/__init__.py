f7ef4bfc3d7104c4e526ebd8eeeeed62891659f0
Henning Oppel <henningurama@gmail.com>
2017-07-31 09:57:59 +0200

    Add files via upload

---
+++ GIS-Tools/Q_prePro1/ACS_preProcessing1/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ ACS_preProcessing1
+                                 A QGIS plugin
+ Transfer stream data
+                             -------------------
+        begin                : 2017-07-26
+        copyright            : (C) 2017 by Laura Bienstein
+        email                : laura.bienstein@rub.de
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load ACS_preProcessing1 class from file ACS_preProcessing1.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .acs_preProcessing1 import ACS_preProcessing1
+    return ACS_preProcessing1(iface)
